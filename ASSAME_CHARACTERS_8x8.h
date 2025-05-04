b198b8ee32581b7cc839f3c70b5b7260975a7a12
robin <rkrobin6550@gmail.com>
2025-05-04 22:13:31 +0530

    added some sample files as example

---
+++ ASSAME_CHARACTERS_8x8.h
@@ -0,0 +1,10 @@
+const u8 font8x8[] = {
+    0x00, 0x00, // size
+    8,         // width
+    8,         // height
+    0xঅ,     // first char
+    1,        // char count
+
+    // Fixed width; char width table not used
+    0x00, 0x00, 0x10, 0xA8, 0x48, 0x7C, 0x06, 0x00, // অ
+};
